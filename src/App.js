f91bbc4fe003a3e8bf0364cf9fcc5dd3512f7127
Prasanta Saha <prasanta.work@outlook.com>
2018-02-20 09:12:01 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Yahoo</div>
+    );
+  }
+}
+
+export default App;
